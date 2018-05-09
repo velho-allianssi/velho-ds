b8ae408f0667459b13d1411d948206af1b310bfa
saara <saara>
2018-05-09 15:01:14 +0300

    poista stylefy/mode, korjaa margin, päivitä stylefy versio

--- src/cljs/velho_ds/atoms/valikot.cljs
+++ src/cljs/velho_ds/atoms/valikot.cljs
@@ -4,26 +4,30 @@
             [velho-ds.tokens.color :as color]
             [velho-ds.tokens.line-height :as lh]))
 
-
-
+(def mode {:hover {:background-color "rgb(98, 131, 213)"}
+           :before{:content "\2228"
+                   :width "50px"
+                   :height "100%"
+                   :line-height lh/line_height_base
+                   :visibility "visible"}
+           :after {:content "\2228"
+                   :width "50px"
+                   :height "100%"
+                   :line-height lh/line_height_base
+                   :visibility "visible"} })
 
 (def alasveto-style {:display "inline-block"
+                     :appearance "none"
                      :background-color "transparent"
                      :border "none"
                      :border-bottom (str "1px solid " color/color_neutral-5)
-                     :color color/color_neutral-4
-                     :padding "10px"
-                     :outline "none"
-                     :appearance "none"
                      :border-radius "0px"
-                     :marging "0px"
-                     ::stylefy/mode {:after {:content "\2228"
-                                             :width "50px"
-                                             :height "100%"
-                                             :line-height lh/line_height_base
-                                             :visibility "visible"} }
+                     :margin "0px"
+                     :outline "none"
+                     :padding "10px"
                      ::stylefy/vendors ["webkit" "moz" "o"]
-                     ::stylefy/auto-prefix #{:border-radius :appearance}})
+                     ::stylefy/auto-prefix #{:border-radius :appearance}
+                     :visibility "visible"})
 
 ;valinta-fn
 (defn alasvetovalikko [data]
