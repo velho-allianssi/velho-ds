13d6b37b07e14aab4863d9a65fae4e3d66f635dc
mikkoro <mikko.ronkkomaki@gmail.com>
2018-05-18 13:35:07 +0300

    Delete old tokens and do not generate new ones from hidden files

--- src/clj/velho_ds/token_loader.clj
+++ src/clj/velho_ds/token_loader.clj
@@ -3,6 +3,8 @@
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
+(def tokens-path "src/cljs/velho_ds/tokens/")
+
 (defn handle-keys [yaml-key data]
   (let [root (yaml-key data)]
     (into {} (for [key (vec (keys root))]
@@ -17,10 +19,11 @@
                (assoc {} key (str "\""(map-aliases aliases (key props)) "\""))))))
 
 (defn read-tokens! [resource]
-  (let [data (yaml/from-file resource)
-        props (handle-keys :props data)
-        aliases (handle-keys :aliases data)]
-    (link-aliases props aliases)))
+  (when (not (.isHidden (io/as-file resource)))
+    (let [data (yaml/from-file resource)
+         props (handle-keys :props data)
+         aliases (handle-keys :aliases data)]
+     (link-aliases props aliases))))
 
 (defn def-ns [filename]
   (str "(ns velho-ds.tokens." filename ")\n\n"))
@@ -30,23 +33,29 @@
 
 (defn write-tokens! [path data]
   (io/make-parents path)
-  (if (.exists (io/as-file path))
-    (io/delete-file path))
+  (let [file (io/as-file path)]
+    (when (.exists file)
+     (io/delete-file path)))
   (doall (for [line (vec data)]
            (spit path line :append true)))
   (println (str "Wrote " (count data) " tokens to a namespace at: " path)))
+
+(defn delete-tokens! [tokens-path]
+  (doseq [f (.listFiles (clojure.java.io/file tokens-path))]
+    (when (.isFile f) (clojure.java.io/delete-file f))))
 
 (defn create-tokens [[filename resource]]
   (let [props (read-tokens! resource)
         nspace (def-ns filename)
         tokens (map (fn [key]
                       (def-token (name key) (key props))) (keys props))]
-    (write-tokens! (str "src/cljs/velho_ds/tokens/" (str/replace filename #"-" "_") ".cljs") (concat (list nspace) tokens))))
+    (write-tokens! (str tokens-path (str/replace filename #"-" "_") ".cljs") (concat (list nspace) tokens))))
 
 (defn get-files-from-path [path]
   (filter #(.isFile %) (file-seq (clojure.java.io/file path))))
 
 (defn create-tokens-from-path [path]
+  (delete-tokens! tokens-path)
   (let [files (get-files-from-path path)
         resource (mapv str (map #(.toPath %) files))
         filenames (mapv str (map #(.getFileName (.toPath %)) files))
