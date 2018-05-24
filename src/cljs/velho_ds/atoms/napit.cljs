1da4f4f42f5b4ba4d41d3add8eb75c7c31b0c49a
Heikki Säily <heikki.saily@solita.fi>
2018-05-24 16:40:37 +0300

    Karttasymboleiden pohjat, tokeneiden päivitys

--- src/cljs/velho_ds/atoms/napit.cljs
+++ src/cljs/velho_ds/atoms/napit.cljs
@@ -4,40 +4,42 @@
             [velho-ds.tokens.font-size :as font-size]
             [velho-ds.tokens.border :as border]
             [velho-ds.tokens.timing :as timing]
+            [velho-ds.tokens.box-shadow :as box-shadow]
+            [velho-ds.tools.measures :as measures]
             [stylefy.core :as stylefy]))
 
 (def nappi {:cursor "pointer"
             :user-select "none"
             :border "none"
-            :font-size "1rem"
-            :padding "1rem"
-            :margin "0 1rem"
+            :font-size font-size/font-size-base
+            :padding spacing/space-small-rem
+            :margin (str "0 " spacing/space-small-rem)
             :box-sizing "border-box"
-            :box-shadow "0px 4px 4px rgba(0,0,0,0.2)"
+            :box-shadow box-shadow/box-shadow
             :color color/color-white
             :background (str "linear-gradient(-45deg," color/color-primary-dark "0%," color/color-primary " 100%)")
             :background-color color/color-primary
             :background-position "left center"
             :background-size "200% auto"
-            :height "64px"
+            :height spacing/space-x-large
             :transition (str "all " timing/duration-slow " ease-in-out")
             ::stylefy/mode {:hover {:background-position "left bottom"
                                     :background-size "200% auto"}
                             :active {:background-position "right center"
                                      :outline "none"
-                                     :box-shadow "0px 2px 2px rgba(0,0,0,0.2)"
+                                     :box-shadow box-shadow/box-shadow-small
                                      :transform "scale(0.95, 0.95)"}
                             :focus {:outline "none"}}
-            ::stylefy/sub-styles {:i {:font-size "1.5rem"
-                                      :vertical-align "-0.4rem"}
-                                  :span {:padding-left "1rem"
-                                         :padding-right "0.5rem"
+            ::stylefy/sub-styles {:i {:font-size font-size/font-size-xx-large
+                                      :vertical-align "-25%"}
+                                  :span {:padding-left spacing/space-small-rem
+                                         :padding-right spacing/space-x-small-rem
                                          :text-decoration "none"
                                          :border "none"}}
             ::stylefy/vendors ["webkit" "moz" "o"]
             ::stylefy/auto-prefix #{:transition}})
 
-(def pieni {:height "32px"
+(def pieni {:height (measures/rem-times spacing/space-small-rem 2)
             :padding "0 1rem"})
 
 (def tumma {:background (str "linear-gradient(-45deg," color/color-neutral-5 "0%," color/color-neutral-4 " 100%)")
@@ -45,16 +47,60 @@
 
 (def kehys {:color color/color-primary
             :background color/color-white
-            :border "1px solid"
+            :border (str border/border-default " solid")
             :border-color color/color-primary
             :background-color color/color-white
             ::stylefy/mode {:hover {:border-color color/color-primary-dark}
                             :active {:background-color color/color-primary
                                      :color color/color-white
                                      :outline "none"
-                                     :box-shadow "0px 2px 2px rgba(0,0,0,0.2)"}
+                                     :box-shadow box-shadow/box-shadow-small}
                             :focus {:outline "none"}}})
 
+(def kartta {:cursor "pointer"
+            :user-select "none"
+            :border (str border/border-default " solid " color/color-neutral-1)
+            :font-size font-size/font-size-base
+            :padding spacing/space-x-small-rem
+            :margin (str "0 " spacing/space-small-rem)
+            :box-sizing "border-box"
+            :box-shadow box-shadow/box-shadow
+            :color color/color-neutral-5
+            :background color/color-white
+            :height spacing/space-large
+            :width spacing/space-large
+            :transition (str "all " timing/duration-slow " ease-in-out")
+            ::stylefy/mode {:hover {:background color/color-neutral-1}
+                            :active {:background color/color-neutral-2
+                                     :box-shadow box-shadow/box-shadow-small
+                                     :transform "scale(0.95, 0.95)"}
+                            :focus {:outline "none"}}
+            ::stylefy/sub-styles {:i {:font-size font-size/font-size-xx-large
+                                      :vertical-align "-25%"}}
+            ::stylefy/vendors ["webkit" "moz" "o"]
+            ::stylefy/auto-prefix #{:transition}})
+
+(def tupla {:user-select "none"
+            :margin "0"
+            :width spacing/space-large })
+
+(def yla {:position "relative"
+          ::stylefy/mode {:hover {:background color/color-neutral-1}
+                          :active {:background color/color-neutral-2
+                                   :transform "translateY(1px) scale(0.95, 0.95)"
+                                   :box-shadow box-shadow/box-shadow-small}
+                          :focus {:outline "none"}}
+          ::stylefy/vendors ["webkit" "moz" "o"]
+          ::stylefy/auto-prefix #{:transition}})
+
+(def ala {:position "relative"
+          ::stylefy/mode {:hover {:background color/color-neutral-1}
+                          :active {:background color/color-neutral-2
+                                   :transform "scale(0.95, 0.95)"
+                                   :box-shadow box-shadow/box-shadow-small}
+                          :focus {:outline "none"}}
+          ::stylefy/vendors ["webkit" "moz" "o"]
+          ::stylefy/auto-prefix #{:transition}})
 
 (defn oletus
   ([content ikoni]
@@ -91,3 +137,16 @@
    [:button (stylefy/use-style (merge nappi kehys pieni))
     (or (not ikoni) [:i.material-icons (stylefy/use-sub-style nappi :i) ikoni])
     [:span (stylefy/use-sub-style nappi (or (not ikoni):span)) content]]))
+
+(defn kartta-ikoni
+  ([ikoni]
+   [:button (stylefy/use-style kartta)
+    [:i.material-icons (stylefy/use-sub-style kartta :i) ikoni]]))
+
+(defn kartta-ikoni-tupla
+  ([ikoni-eka ikoni-toka]
+   [:div (stylefy/use-style tupla)
+    [:button (stylefy/use-style (merge kartta yla))
+     [:i.material-icons (stylefy/use-sub-style kartta :i) ikoni-eka]]
+    [:button (stylefy/use-style (merge kartta ala))
+     [:i.material-icons (stylefy/use-sub-style kartta :i) ikoni-toka]]]))
