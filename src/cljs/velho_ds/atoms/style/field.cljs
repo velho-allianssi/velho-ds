5b4051ca8975e8d0d1f4fc01ea48851f79e66860
mikkoro <mikko.ronkkomaki@gmail.com>
2018-05-29 12:57:39 +0300

    Fix intendations

--- src/cljs/velho_ds/atoms/style/field.cljs
+++ src/cljs/velho_ds/atoms/style/field.cljs
@@ -8,79 +8,86 @@
             [velho-ds.tools.measures :as measures]))
 
 
-(def element {:position "relative"
-                    :display "block"
-                    :min-height spacing/space-x-large
-                    :margin spacing/space-base})
+(def element
+  {:position "relative"
+   :display "block"
+   :min-height spacing/space-x-large
+   :margin spacing/space-base})
 
-(def input-field {:position "absolute"
-                        :top spacing/space-small-rem
-                        :width "100%"
-                        :font-size font-size/font-size-base
-                        :transition (str "border-color " timing/duration-slow " ease-in-out")
-                        :outline "none"
-                        :padding "0"
-                        :margin "0"
-                        :min-height (measures/rem-times font-size/font-size-base 2)
-                        :background "none"
-                        :border-top "0"
-                        :border-left "0"
-                        :border-right "0"
-                        :border-bottom (str border/border-default " solid")
-                        :border-color color/color-neutral-3
-                        ::stylefy/mode {:focus {:border-bottom (str border/border-large " solid")
-                                                :border-color color/color-primary}
-                                        :valid {:border-bottom (str border/border-default " solid")
-                                                :border-color color/color-neutral-3}
-                                        :valid+span {:top "0"
-                                                     :cursor "inherit"
-                                                     :font-size font-size/font-size-small}
-                                        :focus+span {:top "0"
-                                                     :cursor "inherit"
-                                                     :font-size font-size/font-size-small
-                                                     :color color/color-primary}}})
+(def input-field
+  {:position "absolute"
+   :top spacing/space-small-rem
+   :width "100%"
+   :font-size font-size/font-size-base
+   :transition (str "border-color " timing/duration-slow " ease-in-out")
+   :outline "none"
+   :padding "0"
+   :margin "0"
+   :min-height (measures/rem-times font-size/font-size-base 2)
+   :background "none"
+   :border-top "0"
+   :border-left "0"
+   :border-right "0"
+   :border-bottom (str border/border-default " solid")
+   :border-color color/color-neutral-3
+   ::stylefy/mode {:focus {:border-bottom (str border/border-large " solid")
+                           :border-color color/color-primary}
+                   :valid {:border-bottom (str border/border-default " solid")
+                           :border-color color/color-neutral-3}
+                   :valid+span {:top "0"
+                                :cursor "inherit"
+                                :font-size font-size/font-size-small}
+                   :focus+span {:top "0"
+                                :cursor "inherit"
+                                :font-size font-size/font-size-small
+                                :color color/color-primary}}})
 
-(def text-field (merge input-field
-                             {:overflow-y "scroll"
-                              :resize "none"}))
+(def text-field
+  (merge input-field
+         {:overflow-y "scroll"
+          :resize "none"}))
 
-(def input-field-heading {:position "absolute"
-                                :display "block"
-                                :top spacing/space-base-rem
-                                :font-size font-size/font-size-base
-                                :transition (str "all " timing/duration-slow " ease-in-out")
-                                :width "100%"
-                                :cursor "text"})
+(def input-field-heading
+  {:position "absolute"
+   :display "block"
+   :top spacing/space-base-rem
+   :font-size font-size/font-size-base
+   :transition (str "all " timing/duration-slow " ease-in-out")
+   :width "100%"
+   :cursor "text"})
 
-(def dropdown-heading {:position "absolute"
-                             :display "block"
-                             :top "0"
-                             :font-size font-size/font-size-small
-                             :cursor "inherit"
-                             :transition (str "all " timing/duration-slow " ease-in-out")
-                             :width "100%"})
+(def dropdown-heading
+  {:position "absolute"
+   :display "block"
+   :top "0"
+   :font-size font-size/font-size-small
+   :cursor "inherit"
+   :transition (str "all " timing/duration-slow " ease-in-out")
+   :width "100%"})
 
-(def dropdown {:-webkit-appearance "none"
-                     :-moz-appearance "none"
-                     :appearance "none"
-                     :font-family "inherit"
-                     :background-color "transparent"
-                     :width "100%"
-                     :height (measures/rem-times font-size/font-size-base 2)
-                     :padding (str spacing/space-xx-small-rem " 0")
-                     :color color/text-default
-                     :border-top "0"
-                     :border-left "0"
-                     :border-right "0"
-                     :border-bottom (str border/border-default " solid")
-                     :border-color color/color-neutral-3
-                     :border-radius "0"
-                     :margin-top spacing/space-small-rem
-                     :font-size font-size/font-size-base
-                     ::stylefy/mode {:focus {:outline "none"}}})
+(def dropdown
+  {:-webkit-appearance "none"
+   :-moz-appearance "none"
+   :appearance "none"
+   :font-family "inherit"
+   :background-color "transparent"
+   :width "100%"
+   :height (measures/rem-times font-size/font-size-base 2)
+   :padding (str spacing/space-xx-small-rem " 0")
+   :color color/text-default
+   :border-top "0"
+   :border-left "0"
+   :border-right "0"
+   :border-bottom (str border/border-default " solid")
+   :border-color color/color-neutral-3
+   :border-radius "0"
+   :margin-top spacing/space-small-rem
+   :font-size font-size/font-size-base
+   ::stylefy/mode {:focus {:outline "none"}}})
 
-(def icon {:position "absolute"
-                 :padding spacing/space-xx-small-rem
-                 :top spacing/space-small-rem
-                 :right spacing/space-xx-small-rem
-                 :pointer-events "none"})
+(def icon
+  {:position "absolute"
+   :padding spacing/space-xx-small-rem
+   :top spacing/space-small-rem
+   :right spacing/space-xx-small-rem
+   :pointer-events "none"})
