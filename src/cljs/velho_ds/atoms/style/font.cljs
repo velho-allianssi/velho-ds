975fd99a9f01be4077532a7106705b0f35b7a49d
Heikki Säily <heikki.saily@solita.fi>
2018-05-31 17:41:05 +0300

    Font-family update

--- src/cljs/velho_ds/atoms/style/font.cljs
+++ src/cljs/velho_ds/atoms/style/font.cljs
@@ -4,25 +4,31 @@
 
 
 (def p
-  {:font-size font-size/font-size-base
+  {:font-family font/font-family-text
+   :font-size font-size/font-size-base
    :font-weight font/font-weight-base})
 
 (def small
-  {:font-size font-size/font-size-small
+  {:font-family font/font-family-text
+   :font-size font-size/font-size-small
    :font-weight font/font-weight-base})
 
 (def h1
-  {:font-size font-size/font-size-xx-large
+  {:font-family font/font-family-text
+   :font-size font-size/font-size-xx-large
    :font-weight font/font-weight-bold})
 
 (def h2
-  {:font-size font-size/font-size-x-large
+  {:font-family font/font-family-heading
+   :font-size font-size/font-size-x-large
    :font-weight font/font-weight-light})
 
 (def h3
-  {:font-size font-size/font-size-large
+  {:font-family font/font-family-text
+   :font-size font-size/font-size-large
    :font-weight font/font-weight-bold})
 
 (def h4
-  {:font-size font-size/font-size-base
+  {:font-family font/font-family-text
+   :font-size font-size/font-size-base
    :font-weight font/font-weight-bold})
