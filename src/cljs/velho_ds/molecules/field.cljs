bb21a6f7649cd3024fd109b8d287e5aff381300d
Heikki Säily <heikki.saily@solita.fi>
2018-06-04 16:12:34 +0300

    Fields -> molecules siirto

--- src/cljs/velho_ds/molecules/field.cljs
+++ src/cljs/velho_ds/molecules/field.cljs
@@ -3,6 +3,9 @@
             [stylefy.core :as stylefy]
             [velho-ds.molecules.style.field :as style]))
 
+(defn merge-styles [a b]
+  {:style (merge (:style a) (:style b))})
+
 (defn keyvalue [{:keys [content label]}]
  [:div
   [:small (stylefy/use-style style/keyvalue-label) label]
@@ -13,3 +16,46 @@
  [:div
   (if icon [:i.material-icons (stylefy/use-style style/labeless-icon) icon])
   [:p (stylefy/use-style style/labeless-content) content]])
+
+(defn input-field [{:keys [content label on-change-fn]}]
+  (let [input-text (r/atom content)
+        update-and-send (fn [val]
+                (reset! input-text val)
+                (on-change-fn @input-text))]
+    (fn []
+      [:div
+       [:label (stylefy/use-style style/element)
+        [:input (stylefy/use-style style/input-field {:required "required"
+                                                      :on-change #(-> % .-target .-value update-and-send)
+                                                      :value @input-text})]
+        [:span (stylefy/use-style style/input-field-heading) label]]])))
+
+(defn multiline-field
+  ([content]
+   (multiline-field content nil))
+  ([content args]
+   [:div
+    [:label (stylefy/use-style style/element)
+     [:textarea (stylefy/use-style style/text-field {:required "required"})]
+     [:span (stylefy/use-style style/input-field-heading) content]]]))
+
+(defn dropdown-menu [{:keys [heading selected-fn options default-value no-selection-text]}]
+  [:div
+   [:label (stylefy/use-style style/element)
+    (into [:select (stylefy/use-style
+                     style/dropdown
+                     {:on-change #(-> % .-target .-value selected-fn)})
+           (when (not default-value)
+             [:option
+              {:value "value"
+               :selected "selected"
+               :disabled "disabled"}
+              no-selection-text])
+           (stylefy/use-style style/dropdown)]
+          (mapv #(vector :option (merge {:value (:id %)}
+                                        (when (= default-value %)
+                                          {:selected "selected"}))
+                         (:value %))
+                options))
+    [:span (stylefy/use-style style/dropdown-heading) heading]
+    [:i.material-icons (stylefy/use-style style/icon) "arrow_drop_down"]]])
