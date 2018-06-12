83c8bdabb5e6b95ca457f65fe0856759db7aa25a
mikkoro <mikko.ronkkomaki@gmail.com>
2018-06-12 16:27:53 +0300

    Fiddle with the upstreams

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-echo Test! 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+
+git remote add ds-upstream https://${GH_TOKEN}@github.com/velho-allianssi/velho-ds.git > /dev/null 2>&1
+
 git pull
 mkdir -p docs
 cp -r ./public ./docs
 git add docs
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-pages https://${GH_TOKEN}@github.com/velho-allianssi/velho-ds.git > /dev/null 2>&1
-git push --quiet --set-upstream origin-pages master
+git push --quiet --set-upstream ds-upstream master
