43fcbaa1263b35ffca9e8571a61edd643bae2360
mikkoro <mikko.ronkkomaki@gmail.com>
2018-06-12 15:34:36 +0300

    Try to push build artifacts to GH Pages

---
+++ .travis/push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git pull
+mkdir -p docs
+cp -r ./public ./docs
+git add docs
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin-pages https://${GH_TOKEN}@github.com/velho-allianssi/velho-ds/resources.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages master
