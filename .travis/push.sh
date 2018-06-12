d6223d00cbe090d8454bf9630b7af9d7b2d903e1
mikkoro <mikko.ronkkomaki@gmail.com>
2018-06-12 17:17:17 +0300

    Correct branch

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,10 +2,13 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git remote add ds-origin https://${GH_TOKEN}@github.com/velho-allianssi/velho-ds.git > /dev/null 2>&1
+git checkout -b master --track ds-origin/master
+
 mkdir -p docs
 cp -r ./public ./docs
 git add docs
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add ds-origin https://${GH_TOKEN}@github.com/velho-allianssi/velho-ds.git > /dev/null 2>&1
-git push --quiet --set-upstream ds-origin master
+git push
+
