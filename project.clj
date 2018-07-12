11706db91b78cf89c46f8910f717a185c28d3613
mikkoro <mikko.ronkkomaki@gmail.com>
2018-07-12 12:07:35 +0300

    Fix build configs for Figwheel

--- project.clj
+++ project.clj
@@ -36,26 +36,24 @@
 
   :figwheel {:http-server-root "."
              :css-dirs ["public/css"]}
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs" "env/dev/cljs"]
-                        :compiler {:main "velho-ds.dev"
-                                   :output-to "public/js/app.js"
-                                   :output-dir "public/js/out"
-                                   :asset-path "js/out"
-                                   :source-map-timestamp true
-                                   :optimizations :none
-                                   :pretty-print true}
-                        :figwheel {:open-urls ["http://localhost:3449/index.html"]}}
-                       {:id "test"
-                        :source-paths ["src/cljs" "env/dev/cljs" "test/cljs"]
-                        :compiler {:output-to "public/js/test/tests.js"
-                                   :output-dir "public/js/test/out"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}
-                       {:id "release"
-                        :source-paths ["src/cljs" "env/dev/cljs" "test/cljs"]
-                        :compiler {:output-to "target/js/app.js"
-                                   :optimizations :advanced}}]}
+  :cljsbuild {:builds {:dev {:source-paths ["src/cljs" "env/dev/cljs"]
+                             :compiler {:main "velho-ds.dev"
+                                        :output-to "public/js/app.js"
+                                        :output-dir "public/js/out"
+                                        :asset-path "js/out"
+                                        :source-map true
+                                        :optimizations :none
+                                        :pretty-print true}
+                             :figwheel {:on-jsload "velho-ds.core/mount-root"
+                                        :open-urls ["http://localhost:3449/index.html"]}}
+                       :test {:source-paths ["src/cljs" "env/dev/cljs" "test/cljs"]
+                              :compiler {:output-to "public/js/test/tests.js"
+                                         :output-dir "public/js/test/out"
+                                         :optimizations :whitespace
+                                         :pretty-print true}}
+                       :release {:source-paths ["src/cljs" "env/dev/cljs" "test/cljs"]
+                                 :compiler {:output-to "target/js/app.js"
+                                            :optimizations :advanced}}}}
 
   :aliases {"dev" ["do" "clean"
                    ["figwheel"]]
