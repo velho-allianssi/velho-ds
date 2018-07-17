7b74e30983b77830a73f715324dac0b9aab88e37
Tuomas Rinne <tuomas.rinne@solita.fi>
2018-07-17 19:09:57 +0300

    Create navigation based on generated routes

--- src/cljs/velho_ds/core.cljs
+++ src/cljs/velho_ds/core.cljs
@@ -6,7 +6,8 @@
             [velho-ds.pages.main :as main]
             [velho-ds.atoms.font :as font]
             [velho-ds.design-system :as design-system]
-            [velho-ds.routes :refer [routes]]))
+            [velho-ds.routes :refer [routes]]
+            [clojure.string :as str]))
 
 ;; -------------------------
 ;; Navigation
@@ -18,21 +19,34 @@
     (session/put! :route {:current-page (:handler match)
                           :route-params (:route-params match)})))
 
-(def nav
-  [:ul
-   [:li
-    [:a {:href "/"} "Home"]]
-   [:li
-    [:a {:href "/atoms/buttons"} "Buttons"]]
-   [:li
-    [:a {:href "/atoms/fonts"} "Fields"]]])
+(declare navigation)
+(defn subnav [first-item last-item]
+  (let [open (r/atom false)]
+    [:li
+     [:span (str/capitalize first-item)]
+     (navigation last-item)]))
+
+(defn navigation [navigation-routes]
+  (into
+    [:ul]
+    (for [route navigation-routes]
+      (let [first-item (str/replace (first route) #"/" "")
+            last-item (last route)]
+        (if (map? last-item)
+          [subnav first-item last-item]
+          [:li
+           [:a
+            {:href (bidi/path-for routes last-item)}
+            (if (= :index last-item)
+              "Home"
+              (str/capitalize (name last-item)))]])))))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
   (font/font-styles)
-  (r/render [main/page nav] (.getElementById js/document "app")))
+  (r/render [main/page (navigation (last routes))] (.getElementById js/document "app")))
 
 (defn init! []
   (design-system/init-ds!)
