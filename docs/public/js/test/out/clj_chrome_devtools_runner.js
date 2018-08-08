829e0455a453b6625057b74c1993bb9e77fec2dc
Travis CI <travis@travis-ci.org>
2018-08-08 13:31:35 +0000

    Travis build: 251

--- docs/public/js/test/out/clj_chrome_devtools_runner.js
+++ docs/public/js/test/out/clj_chrome_devtools_runner.js
@@ -42,25 +42,25 @@ cljs.test.set_env_BANG_.call(null,env43522);
 
 cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Symbol(null,"velho-ds.atoms.button-test","velho-ds.atoms.button-test",1451672919,null),new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"begin-test-ns","begin-test-ns",-1701237033)], null));
 
-return cljs.test.block.call(null,(function (){var env__42791__auto__ = cljs.test.get_current_env.call(null);
-return cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42791__auto__,env43522,summary43523){
+return cljs.test.block.call(null,(function (){var env__42789__auto__ = cljs.test.get_current_env.call(null);
+return cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42789__auto__,env43522,summary43523){
 return (function (){
-if((env__42791__auto__ == null)){
+if((env__42789__auto__ == null)){
 cljs.test.set_env_BANG_.call(null,cljs.test.empty_env.call(null));
 } else {
 }
 
 
 return null;
-});})(env__42791__auto__,env43522,summary43523))
-], null),cljs.test.test_vars_block.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Var(function(){return velho_ds.atoms.button_test.default_button;},new cljs.core.Symbol("velho-ds.atoms.button-test","default-button","velho-ds.atoms.button-test/default-button",1228505490,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"velho-ds.atoms.button-test","velho-ds.atoms.button-test",1451672919,null),new cljs.core.Symbol(null,"default-button","default-button",302528871,null),"/home/travis/build/velho-allianssi/velho-ds/test/cljs/velho_ds/atoms/button_test.cljs",24,1,4,4,cljs.core.List.EMPTY,null,(cljs.core.truth_(velho_ds.atoms.button_test.default_button)?velho_ds.atoms.button_test.default_button.cljs$lang$test:null)]))], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42791__auto__,env43522,summary43523){
+});})(env__42789__auto__,env43522,summary43523))
+], null),cljs.test.test_vars_block.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Var(function(){return velho_ds.atoms.button_test.default_button;},new cljs.core.Symbol("velho-ds.atoms.button-test","default-button","velho-ds.atoms.button-test/default-button",1228505490,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"velho-ds.atoms.button-test","velho-ds.atoms.button-test",1451672919,null),new cljs.core.Symbol(null,"default-button","default-button",302528871,null),"/home/travis/build/velho-allianssi/velho-ds/test/cljs/velho_ds/atoms/button_test.cljs",24,1,4,4,cljs.core.List.EMPTY,null,(cljs.core.truth_(velho_ds.atoms.button_test.default_button)?velho_ds.atoms.button_test.default_button.cljs$lang$test:null)]))], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42789__auto__,env43522,summary43523){
 return (function (){
-if((env__42791__auto__ == null)){
+if((env__42789__auto__ == null)){
 return cljs.test.clear_env_BANG_.call(null);
 } else {
 return null;
 }
-});})(env__42791__auto__,env43522,summary43523))
+});})(env__42789__auto__,env43522,summary43523))
 ], null));
 })());
 });})(env43522,summary43523))
@@ -78,25 +78,25 @@ cljs.test.set_env_BANG_.call(null,env43522);
 
 cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Symbol(null,"velho-ds.molecules.field-test","velho-ds.molecules.field-test",-1919873050,null),new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"begin-test-ns","begin-test-ns",-1701237033)], null));
 
-return cljs.test.block.call(null,(function (){var env__42791__auto__ = cljs.test.get_current_env.call(null);
-return cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42791__auto__,env43522,summary43523){
+return cljs.test.block.call(null,(function (){var env__42789__auto__ = cljs.test.get_current_env.call(null);
+return cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42789__auto__,env43522,summary43523){
 return (function (){
-if((env__42791__auto__ == null)){
+if((env__42789__auto__ == null)){
 cljs.test.set_env_BANG_.call(null,cljs.test.empty_env.call(null));
 } else {
 }
 
 
 return cljs.test.update_current_env_BANG_.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"each-fixtures","each-fixtures",802243977)], null),cljs.core.assoc,new cljs.core.Symbol(null,"velho-ds.molecules.field-test","velho-ds.molecules.field-test",-1919873050,null),velho_ds.molecules.field_test.cljs_test_each_fixtures);
-});})(env__42791__auto__,env43522,summary43523))
-], null),cljs.test.test_vars_block.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Var(function(){return velho_ds.molecules.field_test.file_list_item;},new cljs.core.Symbol("velho-ds.molecules.field-test","file-list-item","velho-ds.molecules.field-test/file-list-item",-105030888,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"velho-ds.molecules.field-test","velho-ds.molecules.field-test",-1919873050,null),new cljs.core.Symbol(null,"file-list-item","file-list-item",-1854005916,null),"/home/travis/build/velho-allianssi/velho-ds/test/cljs/velho_ds/molecules/field_test.cljs",24,1,12,12,cljs.core.List.EMPTY,null,(cljs.core.truth_(velho_ds.molecules.field_test.file_list_item)?velho_ds.molecules.field_test.file_list_item.cljs$lang$test:null)]))], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42791__auto__,env43522,summary43523){
+});})(env__42789__auto__,env43522,summary43523))
+], null),cljs.test.test_vars_block.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Var(function(){return velho_ds.molecules.field_test.file_list_item;},new cljs.core.Symbol("velho-ds.molecules.field-test","file-list-item","velho-ds.molecules.field-test/file-list-item",-105030888,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"velho-ds.molecules.field-test","velho-ds.molecules.field-test",-1919873050,null),new cljs.core.Symbol(null,"file-list-item","file-list-item",-1854005916,null),"/home/travis/build/velho-allianssi/velho-ds/test/cljs/velho_ds/molecules/field_test.cljs",24,1,12,12,cljs.core.List.EMPTY,null,(cljs.core.truth_(velho_ds.molecules.field_test.file_list_item)?velho_ds.molecules.field_test.file_list_item.cljs$lang$test:null)]))], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__42789__auto__,env43522,summary43523){
 return (function (){
-if((env__42791__auto__ == null)){
+if((env__42789__auto__ == null)){
 return cljs.test.clear_env_BANG_.call(null);
 } else {
 return null;
 }
-});})(env__42791__auto__,env43522,summary43523))
+});})(env__42789__auto__,env43522,summary43523))
 ], null));
 })());
 });})(env43522,summary43523))
