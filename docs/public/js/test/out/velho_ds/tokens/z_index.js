10f0655ef501c57001e57a8e20d407e97b3fb64e
Travis CI <travis@travis-ci.org>
2018-09-03 12:30:45 +0000

    Travis build: 333

--- docs/public/js/test/out/velho_ds/tokens/z_index.js
+++ docs/public/js/test/out/velho_ds/tokens/z_index.js
@@ -3,3 +3,5 @@ goog.provide('velho_ds.tokens.z_index');
 goog.require('cljs.core');
 velho_ds.tokens.z_index.z_index_modal = "9999";
 velho_ds.tokens.z_index.z_index_sticky = "100";
+velho_ds.tokens.z_index.z_index_docked = "4";
+velho_ds.tokens.z_index.z_index_default = "1";
