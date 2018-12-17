b08afc0160aa689df17c5cc5e77df3ffe396cf1e
Heikki Säily <heikki.saily@solita.fi>
2018-12-17 14:49:07 +0200

    Version up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject velho-ds "0.0.0.44"
+(defproject velho-ds "0.0.0.45"
   :description "Velho Allianssi Design System"
   :url "https://github.com/trinne/velho-ds"
   :license {:name "Eclipse Public License"
