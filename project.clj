c8214f0618f69deab7c45f5b39a2fdd68410ebbe
Kimmo Rantala <kimmo.rantala@solita.fi>
2019-04-11 15:49:00 +0300

    fixed icons markup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject velho-ds "0.0.0.65"
+(defproject velho-ds "0.0.0.66"
   :description "Velho Allianssi Design System"
   :url "https://github.com/velho-allianssi/velho-ds"
   :license {:name "Eclipse Public License"
