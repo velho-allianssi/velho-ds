a2efc4657b7795d05aa324f0b4252d3963a4b902
mikkoro <mikko.ronkkomaki@gmail.com>
2019-07-12 15:19:33 +0300

    Pump up the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject velho-ds "0.0.0.73"
+(defproject velho-ds "0.0.0.74"
   :description "Velho Allianssi Design System"
   :url "https://github.com/velho-allianssi/velho-ds"
   :license {:name "Eclipse Public License"
