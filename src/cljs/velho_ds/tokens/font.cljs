e481ea7a5e80cd9545213b4b94f83f30bee20847
Heikki <heikki.saily@gmail.com>
2019-08-27 10:31:12 +0300

    -Update to JDK11 -Update dependencies 	-removed regent and react-dom exclusions -Added managed-dependencies -Set min-lein-version to 2.8.0 -Version up to 0.0.0.78

--- src/cljs/velho_ds/tokens/font.cljs
+++ src/cljs/velho_ds/tokens/font.cljs
@@ -3,6 +3,6 @@
 (def font-family-heading "Montserrat, sans-serif")
 (def font-family-text "Roboto, sans-serif")
 (def font-weight-bold "700")
-(def font-weight-semi-bold "600")
+(def font-weight-semi-bold "500")
 (def font-weight-base "400")
 (def font-weight-light "300")
