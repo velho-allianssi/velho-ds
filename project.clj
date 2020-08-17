4dbe5f5079ea2401f3a1bb6556e81cb81be1bdd7
Erkki Mattila <erkki.mattila@solita.fi>
2020-08-17 17:18:06 +0300

    Bump up version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject velho-ds "0.0.0.86"
+(defproject velho-ds "0.0.0.87"
   :description "Velho Allianssi Design System"
   :url "https://github.com/velho-allianssi/velho-ds"
   :license {:name "Eclipse Public License"
